aa4ca45e6f382364815f19e0d8e3251ac8e39cad
sami-aburwaise <tarrary1@gmail.com>
2023-10-22 11:08:15 +0300

    get coaches

--- GAME-ON/src/App.jsx
+++ GAME-ON/src/App.jsx
@@ -3,8 +3,21 @@ import Home from './pages/Home'
 import Nav from './components/Nav'
 import Footer from './components/Footer'
 import { Routes, Route } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+import { BASE_URL } from '../Globals'
 
 const App = () => {
+  const [coaches, setCoaches] = useState(null)
+
+  const getCoaches = async () => {
+    let response = await axios.get(`${BASE_URL}/show_coach`)
+    setCoaches(response.data)
+  }
+
+  useEffect(() => {
+    getCoaches()
+  }, [])
   return (
     <div id="app">
       <Nav />
